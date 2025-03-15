1c389c37e42184b73f29472ce9aed631de864b89
Harold Hernandez <harold.hernandez@visma.com>
2025-03-15 22:59:12 +0100

    Working metrics, reset endpoints and middleware to increment hits

--- main.go
+++ main.go
@@ -1,26 +1,62 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+
+	return http.HandlerFunc(func (res http.ResponseWriter, req *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(res, req)
+	})
+}
+
 
 func main() {
 	mux := http.NewServeMux()
+	
+	sessionConfig := &apiConfig {
+		fileserverHits: atomic.Int32{},
+	}
 	handler := &http.Server{
 		Addr: ":8080",
 		Handler: mux,
 	}
 
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	fileServerHandler := http.FileServer(http.Dir("."))
+	strippedPrefixHandler := http.StripPrefix("/app", fileServerHandler)
+
+	mux.Handle("/app/", sessionConfig.middlewareMetricsInc(strippedPrefixHandler))
 
 	mux.HandleFunc("/healthz", handlerHealth)
+	mux.HandleFunc("/metrics", sessionConfig.handlePrintMetrics)
+	mux.HandleFunc("/reset", sessionConfig.handleResetMetrics)
 
 	listenAndServeErr := handler.ListenAndServe()
 
 	if listenAndServeErr != nil {
 		log.Fatalf("could not start server: %v", listenAndServeErr)
 	}
+}
+
+func (cfg *apiConfig) handlePrintMetrics(res http.ResponseWriter, req *http.Request) {
+	header := res.Header()
+	header.Set("Content-Type", "text/plain; charset=utf-8")
+	res.WriteHeader(http.StatusOK)
+	content := fmt.Sprintf("Hits: %d\n", cfg.fileserverHits.Load())
+	res.Write([]byte(content))
+}
+
+func (cfg *apiConfig) handleResetMetrics(res http.ResponseWriter, req *http.Request) {
+	cfg.fileserverHits = atomic.Int32{}
 }
 
 func handlerHealth(res http.ResponseWriter, req *http.Request) {
